d761084ceaf2e7aa08756d2286c621637bd40c46
github username <github email address>
2018-06-17 22:28:44 -0700

    created InputForm component

---
+++ client/src/components/InputForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InputForm = (props) =>
+  <form>
+    <label>Input Number:</label>
+    <input type="number" name="input-number"/>
+  </form>
+
+  export default InputForm;
