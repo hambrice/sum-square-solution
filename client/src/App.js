f8e964b042e4bacb67fc6929761bab6f4a178e0b
github username <github email address>
2018-06-17 22:46:25 -0700

    created Display table component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 import InputForm from './components/InputForm';
+import Display from './components/Display';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <InputForm />
+        <Display />
       </div>
     );
   }
---
+++ client/src/components/Display.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Display = (props) =>
+  <div>
+    <table>
+      <tr>
+        <th>Current Time</th>
+        <th>Input</th>
+        <th>Result</th>
+        <th>Occurrences</th>
+        <th>Time of Last Occurrence</th>
+      </tr>
+    </table>
+  </div>
+
+  export default Display;
